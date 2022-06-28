e06d763c1693d29ea1e08dfaed97694648c913eb
Abhishek Jaiswal <abhishek@Abhisheks-MacBook-Air.local>
2022-06-28 20:38:25 +0530

    ctx

--- models/ShopifyV2AHUP/Transactions.js
+++ models/ShopifyV2AHUP/Transactions.js
@@ -6,12 +6,10 @@ const ModelSchema = new Schema({
   jobId: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date },
-  products: [
-    {
-      id: { type: String },
-      title: { type: String }
-    }
-  ]
+  product: {
+    id: { type: String },
+    title: { type: String }
+  }
 })
 
 module.exports = mongoose.model('ShopifyV2AHUPTransactions', ModelSchema, 'shopify-v2ahup-transactions')

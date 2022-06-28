0d46094f42aadd40d41d5f3361662324aa2b67cd
Abhishek Jaiswal <abhishek@Abhisheks-MacBook-Air.local>
2022-06-28 20:12:12 +0530

    ctx

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.Stores = require('./models/Stores')
 module.exports.Shopify_V2AHUP_Workers = require('./models/ShopifyV2AHUP/Workers')
 module.exports.Shopify_V2AHUP_Jobs = require('./models/ShopifyV2AHUP/Jobs')
+module.exports.Shopify_V2AHUP_Transactions = require('./models/ShopifyV2AHUP/Transactions')

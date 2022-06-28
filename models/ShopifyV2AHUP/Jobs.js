4fc6d470e5bde68cf6389e530752ade40fb492c8
Abhishek Jaiswal <abhishek@Abhisheks-MacBook-Air.local>
2022-06-28 20:06:47 +0530

    ctx

--- models/ShopifyV2AHUP/Jobs.js
+++ models/ShopifyV2AHUP/Jobs.js
@@ -6,10 +6,12 @@ const ModelSchema = new Schema({
   workerId: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date },
-  status: { type: String, enum: ['in_progress', 'completed'] },
-  actionType: { type: String, enum: ['hider', 'publisher'] },
-  frequencyType: { type: String, enum: ['manual', 'scheduled', 'instant', 'after_time', 'date_time'], required: true },
-  channelType: { type: String, enum: ['online_store', 'status'], required: true }
+  status: { type: String, enum: ['in_progress', 'completed'], required: true },
+  actionType: { type: String, enum: ['hider', 'publisher'], required: true },
+  frequencyType: { type: String, enum: ['manual', 'scheduled', 'instant', 'after_time', 'date_time'] },
+  channelType: { type: String, enum: ['online_store', 'status'] },
+  iterationCount: { type: Number, default: 0 },
+  completed_at: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('ShopifyV2AHUPJobs', ModelSchema, 'shopify-v2ahup-jobs')
